6aa3b53dbe35724de3cd1687988e1f8b80183239
chandu078 <chandudyavanapelli03@gmail.com>
2025-12-12 16:20:56 +0000

    corvette: Drop High touch polling rate and GloveMode feature
    Quit sadly, this device does not support these feature :(
    
    Change-Id: Idf5848c5f0a2a76fbfbbde25cf75bb7fe9e8b125

--- device.mk
+++ device.mk
@@ -40,7 +40,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # Touch features
-$(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_GM,true)
+$(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_HTPR,false)
 
 # Vibrator
 PRODUCT_PACKAGES += \
