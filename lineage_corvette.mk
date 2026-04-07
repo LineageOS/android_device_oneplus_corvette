e59137155b174367678d85b462ecb6466cc678db
chandu078 <chandudyavanapelli03@gmail.com>
2026-04-07 09:03:38 +0000

    corvette: Update from COS 11.F.15
    Change-Id: I30b72111ba322bda9d4cc2a6a0e0590777c92086

--- lineage_corvette.mk
+++ lineage_corvette.mk
@@ -23,8 +23,8 @@ PRODUCT_MODEL := PJX110
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi-user 16 BP2A.250605.015 1768876029302 release-keys" \
-    BuildFingerprint=OnePlus/PJX110/OP5D06L1:16/UKQ1.231108.001/U.26b348e-47d676-482720:user/release-keys \
+    BuildDesc="qssi-user 16 BP2A.250605.015 1769395211952 release-keys" \
+    BuildFingerprint=OnePlus/PJX110/OP5D06L1:16/UKQ1.231108.001/U.47fa381-257d1ef-25c9613:user/release-keys \
     DeviceName=OP5D06L1 \
     DeviceProduct=PJX110 \
     SystemDevice=OP5D06L1 \
